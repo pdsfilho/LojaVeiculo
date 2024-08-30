4890213c659dba9326e26493cc9da1147229ef6e
Junior Camara <juniorcamara.1@hotmail.com>
2024-08-30 16:26:29 -0300

    Foi alterado a consulta para consultar por RG e N de registro, como também foi acrescentado alguns validadores

--- src/br/com/java1/controllers/ClienteController.java
+++ src/br/com/java1/controllers/ClienteController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 import br.com.java1.models.Cliente;
-import br.com.java1.models.Vendedor;
+import br.com.java1.utils.MetodosDeAlteracao;
+import br.com.java1.utils.Validadores;
+import br.com.java1.utils.Verificadores;
 
 public class ClienteController {
 	List<Cliente> clientes = new ArrayList<Cliente>();
@@ -22,73 +24,69 @@ public class ClienteController {
 		Cliente cliente = new Cliente();
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("ID: ");
-		int id = sc.nextInt();
+		int id = 0;
+		boolean entradaValida = false;
+
+		while (entradaValida == false) {
+			try {
+				System.out.println("ID: ");
+				id = sc.nextInt();
+				entradaValida = true;
+			} catch (java.util.InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("Digite apenas números\n");
+			}
+		}
 		sc.nextLine();
+		if (Verificadores.verificarID(id, clientes) == true) {
+			return;
+		}
 		cliente.setId(id);
 
-		for (Cliente xCliente : getClientes()) {
-			if (id == xCliente.getId()) {
-				System.out.println("\nID já cadastrado, tente novamente\n");
-				return;
-			}
+		System.out.println("RG");
+		String rg = sc.nextLine();
+		while (Validadores.validadorDeRegistros(rg) == false) {
+			System.out.println("RG");
+			rg = sc.nextLine();
 		}
 
-		System.out.println("NÚMERO DE REGISTRO");
-		String rg = sc.nextLine();
-		cliente.setRg(rg);
-		for (Cliente xCliente : getClientes()) {
-			if (rg.equals(xCliente.getRg())) {
-				System.out.println("\nRG já cadastrado, tente novamente\n");
-				return;
-			}
+		if (Verificadores.verificarRGcliente(rg, clientes)) {
+			return;
 		}
+		cliente.setRg(rg);
 
 		System.out.print("NOME: ");
 		String nome = sc.nextLine().toUpperCase();
 		cliente.setNome(nome);
+
 		System.out.print("CPF: ");
 		String cpf = sc.nextLine();
-		while (ValidadorCPFeTelefone.validadorCpf(cpf) == false) {
-			System.out.println("CPF inválido, digite novamente");
+		while (Validadores.validadorCpf(cpf) == false) {
 			cpf = sc.nextLine();
+		}
+
+		if (Verificadores.verificarCPF(cpf, clientes) == true) {
+			return;
 		}
 		cliente.setCpf(cpf);
 
-		for (Cliente xCliente : getClientes()) {
-			if (cpf.equals(xCliente.getCpf())) {
-				System.out.println("\nCPF já cadastrado, tente novamente\n");
-				return;
-			}
-		}
-
 		System.out.print("TELEFONE: ");
 		String telefone = sc.nextLine();
-		while (ValidadorCPFeTelefone.validadorTelefone(telefone) == false) {
-			System.out.println("Telefone inválido, digite novamente");
+		while (Validadores.validadorTelefone(telefone) == false) {
 			telefone = sc.nextLine();
 		}
 		cliente.setTelefone(telefone);
+
 		getClientes().add(cliente);
 		System.out.println("\nCadastro realizado\n");
 	}
 
 	public void excluir() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite o CPF do cliente");
-		String cpf = sc.nextLine();
-		int i = -1;
-
-		for (Cliente xCliente : getClientes()) {
-			if (xCliente.getCpf().equals(cpf)) {
-				i = getClientes().indexOf(xCliente);
-				System.out.println("\nDADOS DO CLIENTE\n" + "\nID: " + xCliente.getId() + "\nRG: " + xCliente.getRg()
-						+ "\nNOME: " + xCliente.getNome() + "\nCPF: " + xCliente.getCpf() + "\nTELEFONE: "
-						+ xCliente.getTelefone());
-			}
-		}
-
-		if (i > -1) {
+		System.out.println("Digite o RG do cliente");
+		String rg = sc.nextLine();
+		Cliente cliente = Verificadores.consultarRG(rg, clientes);
+		if (cliente != null) {
 			System.out.println("\nDigite 'S' para excluir ou 'N' para cancelar\n");
 			String resposta = sc.nextLine();
 			while (!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
@@ -96,106 +94,78 @@ public class ClienteController {
 				resposta = sc.nextLine();
 			}
 			if (resposta.equalsIgnoreCase("s")) {
-				getClientes().remove(i);
+				getClientes().remove(cliente);
 				System.out.println("Cliente excluído com sucesso\n");
-			} else {
-				System.out.println("\nVoltando ao menu principal\n");
-				return;
 			}
+		} else {
+			System.out.println("\nVoltando ao menu principal\n");
+			return;
 		}
-
 	}
 
 	public void consultar() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite o CPF: ");
-		String cpf = sc.nextLine();
-		boolean validador = false;
+		System.out.print("Digite o RG: ");
+		String rg = sc.nextLine();
 
-		for (Cliente xCliente : clientes) {
-			if (cpf.equals(xCliente.getCpf())) {
-				System.out.println("\nDADOS DO CLIENTE\n" + "\nID: " + xCliente.getId() + "\nRG: " + xCliente.getRg()
-						+ "\nNOME: " + xCliente.getNome() + "\nCPF: " + xCliente.getCpf() + "\nTELEFONE: "
-						+ xCliente.getTelefone());
-				System.out.println("________________________________\n");
-				validador = true;
-			}
-		}
-
-		if (validador == false) {
-			System.out.println("\nCliente não encontrado\n");
-		}
+		Verificadores.consultarRG(rg, clientes);
 	}
 
 	public void alterarDados() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("CPF: ");
-		String cpf = sc.nextLine();
+		System.out.println("RG: ");
+		String rg = sc.nextLine();
+		int resposta = 0;
+		Cliente cliente = Verificadores.consultarRG(rg, clientes);
+		if (cliente != null) {
+			while (resposta != 5) {
+				System.out.println("1 - RG\n2 - NOME\n3 - CPF\n4 - TELEFONE\n5 - SAIR\n");
+				resposta = sc.nextInt();
 
-		for (Cliente xCliente : getClientes()) {
-			if (cpf.equals(xCliente.getCpf())) {
-				System.out.println("\nDADOS DO CLIENTE\n" + "\nID: " + xCliente.getId() + "\nRG: " + xCliente.getRg()
-						+ "\nNOME: " + xCliente.getNome() + "\nCPF: " + xCliente.getCpf() + "\nTELEFONE: "
-						+ xCliente.getTelefone() + "\n");
-
-				System.out.println("ALTERAÇÃO DE DADOS\n");
-				boolean validador = true;
-				while (validador) {
-					System.out.println("1 - ALTERAR ID\n" + "2 - RG\n" + "3 - NOME\n" + "4 - CPF\n" + "5 - TELEFONE\n"
-							+ "6 - SAIR");
-					int resposta = sc.nextInt();
-					sc.nextLine();
-
-					switch (resposta) {
-					case 1:
-						System.out.println("NOVO ID");
-						int novoId = sc.nextInt();
-						xCliente.setId(novoId);
-						System.out.println("\nID alterado com sucesso\n");
+				switch (resposta) {
+				case 1:
+					String novoRG = MetodosDeAlteracao.alterarRG();
+					if (Validadores.validadorDeRegistros(novoRG) == false) {
 						break;
-
-					case 2:
-						System.out.println("NOVO RG");
-						String novoRg = sc.nextLine();
-						xCliente.setRg(novoRg);
-						System.out.println("\nRG alterado com sucesso\n");
+					} else if (Verificadores.verificarRGcliente(novoRG, clientes) == true) {
 						break;
-
-					case 3:
-						System.out.println("NOVO NOME");
-						String novoNome = sc.nextLine().toUpperCase();
-						xCliente.setNome(novoNome);
-						System.out.println("\nNome alterado com sucesso\n");
-						break;
-
-					case 4:
-						System.out.println("NOVO CPF");
-						String novoCPF = sc.nextLine();
-						xCliente.setCpf(novoCPF);
-						System.out.println("\nCPF alterado com sucesso\n");
-						break;
-
-					case 5:
-						System.out.println("NOVO TELEFONE");
-						String novoTelefone = sc.nextLine();
-						xCliente.setTelefone(novoTelefone);
-						System.out.println("\nTelefone alterado com sucesso\n");
-						break;
-
-					case 6:
-						System.out.println("Saindo...\n");
-						validador = false;
-						return;
-
-					default:
-						System.out.println("Opção inválida, tente novamente");
+					} else {
+						cliente.setRg(novoRG);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
 					}
-
+					break;
+				case 2:
+					String novoNome = MetodosDeAlteracao.alterarNome();
+					cliente.setNome(novoNome);
+					System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					break;
+				case 3:
+					String novoCPF = MetodosDeAlteracao.alterarCPF();
+					if (Validadores.validadorCpf(novoCPF) == false) {
+						break;
+					} else if (Verificadores.verificarCPF(novoCPF, clientes) == true) {
+						break;
+					} else {
+						cliente.setCpf(novoCPF);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					}
+					break;
+				case 4:
+					String novoTelefone = MetodosDeAlteracao.alterarTelefone();
+					if (Validadores.validadorTelefone(novoTelefone) == false) {
+						break;
+					} else {
+						cliente.setTelefone(novoTelefone);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					}
+					break;
+				case 5:
+					System.out.println("Saindo...");
+					break;
+				default:
+					System.out.println("Opção inválida, digite novamente\n");
+					break;
 				}
-
-			} else {
-				System.out.println("Cliente não encontrado");
-				return;
 			}
 		}
 	}
--- src/br/com/java1/controllers/VendedorController.java
+++ src/br/com/java1/controllers/VendedorController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import br.com.java1.models.Cliente;
 import br.com.java1.models.Vendedor;
+import br.com.java1.utils.MetodosDeAlteracao;
+import br.com.java1.utils.Validadores;
+import br.com.java1.utils.Verificadores;
 
 public class VendedorController {
 	List<Vendedor> vendedores = new ArrayList<Vendedor>();
@@ -21,178 +23,147 @@ public class VendedorController {
 	public void cadastrar() {
 		Vendedor vendedor = new Vendedor();
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("ID: ");
-		int id = sc.nextInt();
-		vendedor.setId(id);
-		sc.nextLine();
 
-		for (Vendedor xVendedor : getVendedores()) {
-			if (id == xVendedor.getId()) {
-				System.out.println("\nID já cadastrado, tente novamente\n");
-				return;
+		int id = 0;
+		boolean entradaValida = false;
+		while (entradaValida == false) {
+			try {
+				System.out.println("ID: ");
+				id = sc.nextInt();
+				entradaValida = true;
+			} catch (java.util.InputMismatchException e) {
+				sc.nextLine();
+				System.out.println("Digite apenas números\n");
 			}
 		}
+		sc.nextLine();
+		if (Verificadores.verificarID(id, vendedores) == true) {
+			return;
+		}
+		vendedor.setId(id);
 
 		System.out.println("NÚMERO DE REGISTRO");
 		String numRegistro = sc.nextLine();
-		vendedor.setNumeroRegistro(numRegistro);
-		
-		for (Vendedor xVendedor : getVendedores()) {
-			if (numRegistro.equals(xVendedor.getNumeroRegistro())) {
-				System.out.println("\nNúmero de registro já cadastrado, tente novamente\n");
-				return;
-			}
+		while (Validadores.validadorDeRegistros(numRegistro) == false) {
+			System.out.println("NÚMERO DE REGISTRO");
+			numRegistro = sc.nextLine();
 		}
+		if (Verificadores.verificarNºRegistroVendedor(numRegistro, vendedores)) {
+			return;
+		}
+		vendedor.setNumeroRegistro(numRegistro);
 
 		System.out.print("NOME: ");
 		String nome = sc.nextLine().toUpperCase();
 		vendedor.setNome(nome);
-		
+
 		System.out.print("CPF: ");
 		String cpf = sc.nextLine();
-		while (ValidadorCPFeTelefone.validadorCpf(cpf)==false) {
-			System.out.println("CPF inválido, digite novamente");
+		while (Validadores.validadorCpf(cpf) == false) {
 			cpf = sc.nextLine();
+		}
+		if (Verificadores.verificarCPF(cpf, vendedores) == true) {
+			return;
 		}
 		vendedor.setCpf(cpf);
 
-		for (Vendedor xVendedor : getVendedores()) {
-			if (cpf.equals(xVendedor.getCpf())) {
-				System.out.println("\nCPF já cadastrado, tente novamente\n");
-				return;
-			}
-		}
-
 		System.out.print("TELEFONE: ");
 		String telefone = sc.nextLine();
-		while (ValidadorCPFeTelefone.validadorTelefone(telefone) == false) {
-			System.out.println("Telefone inválido, digite novamente");
+		while (Validadores.validadorTelefone(telefone) == false) {
 			telefone = sc.nextLine();
 		}
 		vendedor.setTelefone(telefone);
-		
+
 		getVendedores().add(vendedor);
 		System.out.println("\nCadastro realizado\n");
 	}
 
 	public void excluir() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite o CPF do vendedor");
-		String cpf = sc.nextLine();
-		int i = -1;
+		System.out.println("Digite o Nº REGISTRO do vendedor");
+		String numRegistro = sc.nextLine();
+		Vendedor vendedor = Verificadores.consultarNumeroRegistro(numRegistro, vendedores);
 
-		for (Vendedor xVendedor : getVendedores()) {
-			if (xVendedor.getCpf().equals(cpf)) {
-				i = getVendedores().indexOf(xVendedor);
-				System.out.println("\nDADOS DO VENDEDOR\n" + "ID: " + xVendedor.getId() + "\nREGISTRO: "
-						+ xVendedor.getNumeroRegistro() + "\nNOME: " + xVendedor.getNome() + "\nCPF: "
-						+ xVendedor.getCpf() + "\nTELEFONE: " + xVendedor.getTelefone());
-				System.out.println("________________________________\n");
-			}
-		}
-
-		if (i > -1) {
-			System.out.println("\nDigite s/n para excluir");
+		if (vendedor != null) {
+			System.out.println("\nDigite 'S' para excluir ou 'N' para cancelar\n");
 			String resposta = sc.nextLine();
-			while(!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
+			while (!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
 				System.out.println("\nOpção inválida, tente novamente\n");
 				resposta = sc.nextLine();
 			}
 			if (resposta.equalsIgnoreCase("s")) {
-				getVendedores().remove(i);
-				System.out.println("\nVendedor excluído\n");
-			} else {
-				System.out.println("\nVoltando ao menu principal\n");
-				return;
+				getVendedores().remove(vendedor);
+				System.out.println("Vendedor excluído com sucesso\n");
 			}
+		} else {
+			System.out.println("\nVoltando ao menu principal\n");
+			return;
 		}
-
 	}
 
 	public void consultar() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite o CPF: ");
-		String cpf = sc.nextLine();
-		boolean validador = false;
+		System.out.print("DIGITE O Nº DE REGISTRO: ");
+		String numRegistro = sc.nextLine();
 
-		for (Vendedor xVendedor : vendedores) {
-			if (xVendedor.getCpf().equals(cpf)) {
-				System.out.println("\nDADOS DO VENDEDOR\n" + "ID: " + xVendedor.getId() + "\nREGISTRO: "
-						+ xVendedor.getNumeroRegistro() + "\nNOME: " + xVendedor.getNome() + "\nCPF: "
-						+ xVendedor.getCpf() + "\nTELEFONE: " + xVendedor.getTelefone());
-				System.out.println("________________________________\n");
-				validador = true;
-			}
-		}
-		if (validador == false) {
-			System.out.println("\nVendedor não encontrado\n");
-		}
+		Verificadores.consultarNumeroRegistro(numRegistro, vendedores);
 	}
 
 	public void alterarDados() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("CPF: ");
-		String cpf = sc.nextLine();
+		System.out.println("Nº REGISTRO: ");
+		String numRegistro = sc.nextLine();
 
-		for (Vendedor xVendedor : getVendedores()) {
-			if (cpf.equals(xVendedor.getCpf())) {
-				System.out.println("\nDADOS DO VENDEDOR\n" + "\nID: " + xVendedor.getId() + "\nNÚMERO DE REGISTRO: "
-						+ xVendedor.getNumeroRegistro() + "\nNOME: " + xVendedor.getNome() + "\nCPF: "
-						+ xVendedor.getCpf() + "\nTELEFONE: " + xVendedor.getTelefone() + "\n");
+		int resposta = 0;
+		Vendedor vendedor = Verificadores.consultarNumeroRegistro(numRegistro, vendedores);
+		if (vendedor != null) {
+			while (resposta != 5) {
+				System.out.println("1 - Nº REGISTRO\n2 - NOME\n3 - CPF\n4 - TELEFONE\n5 - SAIR\n");
+				resposta = sc.nextInt();
 
-				System.out.println("ALTERAÇÃO DE DADOS\n");
-				boolean validador = true;
-				while (validador) {
-					System.out.println("1 - ALTERAR ID\n" + "2 - NÚMERO DE REGISTRO\n" + "3 - NOME\n" + "4 - CPF\n"
-							+ "5 - TELEFONE\n" + "6 - SAIR");
-					int resposta = sc.nextInt();
-					sc.nextLine();
-
-					switch (resposta) {
-					case 1:
-						System.out.println("NOVO ID");
-						int novoId = sc.nextInt();
-						xVendedor.setId(novoId);
-						System.out.println("\nID alterado com sucesso\n");
+				switch (resposta) {
+				case 1:
+					String novoNºRegistro = MetodosDeAlteracao.alterarRG();
+					if (Validadores.validadorDeRegistros(novoNºRegistro) == false) {
 						break;
-
-					case 2:
-						System.out.println("NOVO NUMERO DE REGISTRO");
-						String novoNumRegistro = sc.nextLine();
-						xVendedor.setNumeroRegistro(novoNumRegistro);
-						break;
-
-					case 3:
-						System.out.println("NOVO NOME");
-						String novoNome = sc.nextLine().toUpperCase();
-						xVendedor.setNome(novoNome);
-						System.out.println("\nNome alterado com sucesso\n");
-						break;
-
-					case 4:
-						System.out.println("NOVO CPF");
-						String novoCPF = sc.nextLine();
-						xVendedor.setCpf(novoCPF);
-						System.out.println("\nCPF alterado com sucesso\n");
-						break;
-
-					case 5:
-						System.out.println("NOVO TELEFONE");
-						String novoTelefone = sc.nextLine();
-						xVendedor.setTelefone(novoTelefone);
-						System.out.println("\nTelefone alterado com sucesso\n");
-						break;
-
-					case 6:
-						System.out.println("Saindo...\n");
-						validador = false;
+					} else if (Verificadores.verificarNºRegistroVendedor(novoNºRegistro, vendedores) == true) {
 						return;
-
-					default:
-						System.out.println("Opção inválida");
-						break;
+					} else {
+						vendedor.setNumeroRegistro(novoNºRegistro);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
 					}
+					break;
+				case 2:
+					String novoNome = MetodosDeAlteracao.alterarNome();
+					vendedor.setNome(novoNome);
+					System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					break;
+				case 3:
+					String novoCPF = MetodosDeAlteracao.alterarCPF();
+					if (Validadores.validadorCpf(novoCPF) == false) {
+						break;
+					} else if (Verificadores.verificarCPF(novoCPF, vendedores) == true) {
+						break;
+					} else {
+						vendedor.setCpf(novoCPF);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					}
+					break;
+				case 4:
+					String novoTelefone = MetodosDeAlteracao.alterarTelefone();
+					if (Validadores.validadorTelefone(novoTelefone) == false) {
+						break;
+					} else {
+						vendedor.setTelefone(novoTelefone);
+						System.out.println("ALTERAÇÃO REALIZADA COM SUCESSO\n");
+					}
+					break;
+				case 5:
+					System.out.println("Saindo...\n");
+					break;
+				default:
+					System.out.println("Opção inválida, digite novamente\n");
+					break;
 				}
 			}
 		}
